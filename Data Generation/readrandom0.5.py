366945f30390cbde4afed65eecbe971a90bf41bc
Ronald McCollam <rmccollam@HW11242.udlunder>
2016-04-05 09:32:19 -0400

    Swap latitude and longitude to properly generate points

--- Data Generation/readrandom0.5.py
+++ Data Generation/readrandom0.5.py
@@ -22,6 +22,6 @@ linecc=get_random_line(file_name='name_cc.csv').rstrip('\r\n')
 transaction_comma=linecc+','+linezip
 splitstring=transaction_comma.split(',')
 
-transaction={'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[6]), float(splitstring[7])]}}
+transaction={'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[7]), float(splitstring[6])]}}
 
 sys.stdout.write(json.dumps(transaction))
